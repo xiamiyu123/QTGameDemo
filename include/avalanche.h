10526493272b60452b0d245a8bc47a3e380b714f
13050154790 <487206710@qq.com>
2025-05-22 20:11:21 +0800

    feat: 补回计算雪崩末端与玩家距离的逻辑

--- include/avalanche.h
+++ include/avalanche.h
@@ -22,6 +22,8 @@ public:
     bool isPlayerSurpassed(qreal playerX) const;
     // 获取雪崩前沿与玩家的距离
     qreal distanceToPlayer(qreal playerX) const;
+    // 获取雪崩末端与玩家的距离
+    qreal distanceToPlayerLeft(qreal playerX) const;
 
     // 线程安全的雪崩更新方法
     void updateAvalancheThreadSafe(qreal elapsed, qreal playerX);

a78727d2601f7e8c35a061f5f2aee7e015ad0662
zhanghengyi <487206710@qq.com>
2025-05-23 14:52:12 +0800

    feat: 添加雪崩预警 pref: 首个地形块不生成石头

--- include/gamescene.h
+++ include/gamescene.h
@@ -84,6 +84,7 @@ private:
     QGraphicsTextItem *GPauseText;
     QPushButton *pauseButton;
     QGraphicsRectItem *GPauseOverlay; // 暂停时的渐变遮罩
+    QPushButton *warningButton;
     AvalancheUpdateThread* m_avalancheThread;  // 雪崩更新线程
     QList<IPhysicsObject*> m_objectsToDeleteThisFrame; // 新增：用于存储本帧待删除的对象
 };
